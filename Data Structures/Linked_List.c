8c03f61e3765a17014d4316a56cd5ac8f49d6995
Priyanshu-Prakhar <prakhar2.178@gmail.com>
2026-02-11 00:10:44 +0530

    Initialization of Nodes done

--- Data Structures/Linked_List.c
+++ Data Structures/Linked_List.c
@@ -6,6 +6,12 @@ struct node{
     struct node *link;
 }; // Node of Linked List
 int main(){
+    struct node *head = NULL; // Initialisiing the head node.
+    head = malloc(sizeof(struct node));
 
+    head->data = 45;
+    head->link = NULL; /* All nodes including the head node point to NULL when initialised.
+    This gives us freedom to expand the list later if we need to */
+    
     return 0;
 }
